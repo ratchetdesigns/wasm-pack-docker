c769a2a9106e7b5bbde5cbe1d38c4ce9c2a49b0b
Siva <byteshiva@gmail.com>
2021-11-30 16:16:07 +0530

    upgrade node version,wasm-pack, rust

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-node_version='14.17.4-r0'
-wasm_pack_version='0.10.0'
+node_version='14.18.1-r0'
+wasm_pack_version='0.10.1'
 docker build . --build-arg "NODE_VERSION=${node_version}" --build-arg "WASM_PACK_VERSION=${wasm_pack_version}" -t "ratchetdesigns/wasm-pack:${wasm_pack_version}"
