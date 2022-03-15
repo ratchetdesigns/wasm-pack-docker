d3d586db5ebdb2385e5b2a4feaa1a4a907c7836d
Adam Berger <adam@ratchetdesigns.com>
2022-03-14 20:07:01 -0400

    build: bump versions

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-node_version='14.17.6-r0'
+node_version='14.19.0-r0'
 npm_version='7.17.0-r0'
-wasm_pack_version='0.10.0'
+wasm_pack_version='0.10.2'
 docker build . --build-arg "NODE_VERSION=${node_version}" --build-arg "NPM_VERSION=${npm_version}" --build-arg "WASM_PACK_VERSION=${wasm_pack_version}" -t "ratchetdesigns/wasm-pack:${wasm_pack_version}"
